5755f9eab36260b86e57773ca95734325a6539d5
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-06-02 19:02:16 +1000

    SpreadsheetComparatorProviderPluginProvider
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-plugin/issues/9
    - A PluginProvider that implements and wraps SpreadsheetComparatorProvider

---
+++ src/main/java/walkingkooka/spreadsheet/plugin/SpreadsheetComparatorProviderPluginProvider.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2024 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.plugin;
+
+import walkingkooka.net.AbsoluteUrl;
+import walkingkooka.plugin.PluginInfo;
+import walkingkooka.plugin.PluginName;
+import walkingkooka.plugin.PluginProvider;
+import walkingkooka.plugin.PluginProviderName;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparator;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorInfo;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorName;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorProvider;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * A {@link PluginProvider} that wraps a {@link SpreadsheetComparatorProvider}. The {@link PluginInfo} are created from
+ * the {@link SpreadsheetComparatorProvider#spreadsheetComparatorInfos()}.
+ */
+final class SpreadsheetComparatorProviderPluginProvider implements SpreadsheetComparatorProvider,
+        PluginProvider {
+
+    static SpreadsheetComparatorProviderPluginProvider with(final SpreadsheetComparatorProvider spreadsheetComparatorProvider,
+                                                            final PluginProviderName name,
+                                                            final AbsoluteUrl url) {
+        return new SpreadsheetComparatorProviderPluginProvider(
+                Objects.requireNonNull(spreadsheetComparatorProvider, "spreadsheetComparatorProvider"),
+                Objects.requireNonNull(name, "name"),
+                Objects.requireNonNull(url, "url")
+        );
+    }
+
+    private SpreadsheetComparatorProviderPluginProvider(final SpreadsheetComparatorProvider spreadsheetComparatorProvider,
+                                                        final PluginProviderName name,
+                                                        final AbsoluteUrl url) {
+        this.spreadsheetComparatorProvider = spreadsheetComparatorProvider;
+        this.name = name;
+        this.url = url;
+    }
+
+    @Override
+    public Optional<SpreadsheetComparator<?>> spreadsheetComparator(final SpreadsheetComparatorName name) {
+        return this.spreadsheetComparatorProvider.spreadsheetComparator(name);
+    }
+
+    @Override
+    public Set<SpreadsheetComparatorInfo> spreadsheetComparatorInfos() {
+        return this.spreadsheetComparatorProvider.spreadsheetComparatorInfos();
+    }
+
+    private final SpreadsheetComparatorProvider spreadsheetComparatorProvider;
+
+    // PluginProvider...................................................................................................
+
+    @Override
+    public PluginProviderName name() {
+        return this.name;
+    }
+
+    private final PluginProviderName name;
+
+    @Override
+    public AbsoluteUrl url() {
+        return this.url;
+    }
+
+    private final AbsoluteUrl url;
+
+    @Override
+    public Set<PluginInfo> pluginInfos() {
+        return this.spreadsheetComparatorInfos()
+                .stream()
+                .map(SpreadsheetComparatorProviderPluginProvider::toPlugin)
+                .collect(Collectors.toSet());
+    }
+
+    private static PluginInfo toPlugin(final SpreadsheetComparatorInfo info) {
+        return PluginInfo.with(
+                info.url(),
+                PluginName.with(
+                        info.name()
+                                .value()
+                )
+        );
+    }
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.spreadsheetComparatorProvider.toString();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/plugin/SpreadsheetComparatorProviderPluginProviderTest.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2024 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.plugin;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.ToStringTesting;
+import walkingkooka.collect.set.Sets;
+import walkingkooka.net.AbsoluteUrl;
+import walkingkooka.net.Url;
+import walkingkooka.plugin.PluginInfo;
+import walkingkooka.plugin.PluginName;
+import walkingkooka.plugin.PluginProviderName;
+import walkingkooka.plugin.PluginProviderTesting;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparator;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorInfo;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorName;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorProvider;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorProviderTesting;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorProviders;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparators;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class SpreadsheetComparatorProviderPluginProviderTest implements PluginProviderTesting<SpreadsheetComparatorProviderPluginProvider>,
+        SpreadsheetComparatorProviderTesting<SpreadsheetComparatorProviderPluginProvider>,
+        ToStringTesting<SpreadsheetComparatorProviderPluginProvider> {
+
+    private final static AbsoluteUrl SPREADSHEET_COMPARATOR_INFO_URL = Url.parseAbsolute("https://example.com/SpreadsheetComparatorInfo123");
+    private final static String SPREADSHEET_COMPARATOR_INFO_NAME = "Test456";
+
+    private final static Set<SpreadsheetComparatorInfo> INFOS = Sets.of(
+            SpreadsheetComparatorInfo.with(
+                    SPREADSHEET_COMPARATOR_INFO_URL,
+                    SpreadsheetComparatorName.with(SPREADSHEET_COMPARATOR_INFO_NAME)
+            )
+    );
+
+    private final static SpreadsheetComparatorProvider SPREADSHEET_COMPARATOR_PROVIDER = new SpreadsheetComparatorProvider() {
+        @Override
+        public Optional<SpreadsheetComparator<?>> spreadsheetComparator(final SpreadsheetComparatorName name) {
+            return SpreadsheetComparatorProviders.builtIn().spreadsheetComparator(name);
+        }
+
+        @Override
+        public Set<SpreadsheetComparatorInfo> spreadsheetComparatorInfos() {
+            return INFOS;
+        }
+    };
+
+    private final static PluginProviderName NAME = PluginProviderName.with("Test123");
+
+    private final static AbsoluteUrl URL = Url.parseAbsolute("https://example.com/123");
+
+    @Test
+    public void testWithNullSpreadsheetComparatorProviderFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> SpreadsheetComparatorProviderPluginProvider.with(
+                        null,
+                        NAME,
+                        URL
+                )
+        );
+    }
+
+    @Test
+    public void testWithNullNameFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> SpreadsheetComparatorProviderPluginProvider.with(
+                        SPREADSHEET_COMPARATOR_PROVIDER,
+                        null,
+                        URL
+                )
+        );
+    }
+
+    @Test
+    public void testWithNullUrlFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> SpreadsheetComparatorProviderPluginProvider.with(
+                        SPREADSHEET_COMPARATOR_PROVIDER,
+                        NAME,
+                        null
+                )
+        );
+    }
+
+    @Test
+    public void testSpreadsheetComparatorName() {
+        final SpreadsheetComparator<?> comparator = SpreadsheetComparators.date();
+
+        this.spreadsheetComparatorAndCheck(
+                this.createPluginProvider(),
+                comparator.name(),
+                comparator
+        );
+    }
+
+    @Test
+    public void testSpreadsheetComparatorInfos() {
+        this.spreadsheetComparatorInfosAndCheck(
+                this.createPluginProvider(),
+                SPREADSHEET_COMPARATOR_PROVIDER.spreadsheetComparatorInfos()
+        );
+    }
+
+    @Test
+    public void testPluginInfos() {
+        this.pluginInfosAndCheck(
+                PluginInfo.with(
+                        SPREADSHEET_COMPARATOR_INFO_URL,
+                        PluginName.with(SPREADSHEET_COMPARATOR_INFO_NAME)
+                )
+        );
+    }
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+                this.createPluginProvider(),
+                SPREADSHEET_COMPARATOR_PROVIDER.toString()
+        );
+    }
+
+    // PluginProvider...................................................................................................
+
+    @Override
+    public SpreadsheetComparatorProviderPluginProvider createPluginProvider() {
+        return SpreadsheetComparatorProviderPluginProvider.with(
+                SPREADSHEET_COMPARATOR_PROVIDER,
+                NAME,
+                URL
+        );
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PACKAGE_PRIVATE;
+    }
+
+    @Override
+    public Class<SpreadsheetComparatorProviderPluginProvider> type() {
+        return SpreadsheetComparatorProviderPluginProvider.class;
+    }
+}
